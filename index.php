a2f478b3f9060da1a270da7dc6b28a5c5c6e650c
Erreur32 <postmaster@echosystem.fr>
2021-01-31 14:35:43 +0100

    first commit

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $sec = "300";
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
         <title><?php echo "$title"; ?></title>
-	<meta name='description' content='<?php echo $description; ?>'>
+	<meta name='description' content='<?php echo $descript; ?>'>
 
 	<meta name="author" content="Erreur32">
 	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
