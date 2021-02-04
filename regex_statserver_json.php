0c4d7dda330a1df5d4e7e145ed45a2267580efe3
Erreur32 <postmaster@echosystem.fr>
2021-02-04 20:52:15 +0100

    first commit

--- regex_statserver_json.php
+++ regex_statserver_json.php
@@ -133,5 +133,33 @@ $fl_array = preg_grep("/[^,entm][0-9][.][0-9]{1}/", $InfoGT);
 echo $fl_array;
 */
 
+
+echo "<hr>";
+$re = '/[0-9]{1,2}[:][0-9]{1,2}/m';
+$str = 'battleye,external,privHive,shard,lqs0,etm2.000000,entm5.500000,mod,23:17';
+
+preg_match_all($re, $str, $matches, PREG_SET_ORDER, 0);
+
+// Print the entire match result
+//var_dump($matches);
+
+echo '<pre>'; print_r($matches); echo '</pre>';
+
+echo "<br>";
+
+$encode = json_encode($matches);
+echo $encode;
+echo "<br>";
+
+//echo $matches[0];
+$output = implode(",", array($str));
+echo $output;
+echo "<br>";
+$output1 = implode(",", array($matches));
+echo $output1;
+echo "<br>";
+
+
+
 ?>
 
--- table_insert.sql
+++
@@ -1,6 +0,0 @@
-
- INSERT INTO `StatServer_1` (`id`, `date`, `name`, `numplayers`, `timeserver`, `players`, `map`, `game`, `maxplayers`, `requiredVersion`, `version`, `battleye`, `hive`, `connect`, `secure`, `ping`) VALUES
- (1, '',     '',     '0',    '',     '',     '',     '',     '',     'null', '',     '',     '',     '',     '',     '');
-
-
-
