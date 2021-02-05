a5b8b60d7553b04fb4f4023108bf0f8e55733adf
Erreur32 <erreur32@echosystem.fr>
2021-02-05 22:50:33 +0100

    Upload files to ''

--- index.php
+++ index.php
@@ -84,45 +84,18 @@ $sec = "300";
 			border-radius: .25em;
 		}
 
-		.selection {
-			color: black !important;
-		}
+		.selection {			color: black !important;		}
 
-		.select2-results {
-			color: black !important;
-		}
+		.select2-results {			color: black !important;		}
 
-		.select2-results__option--highlighted {
-			background-color: #3875d7 !important;
-		}
+		.select2-results__option--highlighted {			background-color: #3875d7 !important;		}
 
-		.pull-left {
-			float: left !important;
-		}
+		.pull-left {			float: left !important;		}
 
-		.pull-right {
-			float: right !important;
-		}
+		.pull-right {			float: right !important;		}
 
-		.navbar-brand-logo {
-			margin-left: 2rem;
-		}
+		.navbar-brand-logo {			margin-left: 2rem;		}
 	</style>
-
-	<script>
-			window.dataLayer = window.dataLayer || [];
-			window.mobileAndTabletcheck = function() {
-				var check = false;
-				(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-				return check;
-			};
-			window.mobilecheck = function() {
-				var check = false;
-				(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-				return check;
-			};
-		</script>
-
 
 
 <?php
@@ -131,9 +104,9 @@ $sec = "300";
 		$time = $time[1] + $time[0];
 		$start = $time;
 
-                if (empty($Info['HostName']))  {
-                $Info['HostName'] = "<h1>OFF LINE</h1>" ;
-                }
+        if (empty($Info['HostName']))  {
+        $Info['HostName'] = "<h1>OFF LINE</h1>" ;
+        }
 ?> 
 		<main class="main" style="margin-top: 10px">
 
@@ -163,14 +136,15 @@ $sec = "300";
 
 <?php
 	        if (empty($Info['Map']))  {
-		echo "</main>";
+				echo "</main>";
+
+				// change to our local zone !!
                 setlocale(LC_ALL,'french');
-                echo "<center><small class='text-muted'>Last refresh</small> <br>  <strong class='h4'>".date('m/d/y H:i:s')."</strong></center>";
+
+				echo "<center><small class='text-muted'>Last refresh</small> <br>  <strong class='h4'>".date('m/d/y H:i:s')."</strong></center>";
                 echo "<div style=\"padding-bottom: 10%;padding-left: 20%;padding-right: 20%\"> <center><img src=".$logoteam." class=\"arrondie2\"   width=\"100%\" max-height=\"20%\" height=\"auto\"></center></div></div>";
                 exit;
-
                 }
-
 ?>
 									<div class="card-footer">
 										<div class="row" style="text-align:center;">
@@ -178,11 +152,10 @@ $sec = "300";
 											<div class="col-md-2 col-xs-12 border-right">
 												<div class="description-block">
 													<h5 class="description-header">
-													<?php if (empty($Info['Players'])) {
-														  echo "<span style='color:grey;'>"; } ?>
-                                                                                                         <?php  echo $Info['Players']; ?> <span style='color:grey;'>/<?php echo  $Info['MaxPlayers'] ; ?></span></span>
+													 <?php if (empty($Info['Players'])) { echo "<span style='color:grey;'>"; } ?>
+                                                     <?php echo $Info['Players']; ?> <span style='color:grey;'>/<?php echo $Info['MaxPlayers'] ; ?></span></span>
 													</h5>
-													 <span class="description-text">PLAYERS</span>
+													   <span class="description-text">PLAYERS</span>
 												</div>
 											</div>
 
@@ -205,28 +178,24 @@ $sec = "300";
 												<div class="">
 													<h5 class="description-header">
 										<?php  //echo count($objlower)
-									// NEED to find a good regex for this shit!
+											echo  $timeserver ;
+											echo " <small style='color: grey;'>  <i class='fas  fa-sun'></i> <span style='color: white;'> x ".$timespeed."</span> - <i class='fas  fa-moon'></i><span style='color: white;'> x ".$timespeedn."  </span>";
+
+
+/*								 
 								     if (count($objlower) == 0){      // if no mod
 									 echo $InfoGT[67].$InfoGT[68].$InfoGT[69].$InfoGT[70].$InfoGT[71].$InfoGT[72].$InfoGT[73];  
-						                  	 echo " <small style='color: grey;'>  <i class='fas  fa-sun'></i> <span style='color: white;'> x ". $InfoGT[42]. "</span> - <i class='fas  fa-moon'></i><span style='color: white;'> x ".$InfoGT[55]."  </span>";											} 
+									 echo " <small style='color: grey;'>  <i class='fas  fa-sun'></i> <span style='color: white;'> x ". $InfoGT[42]. "</span> - <i class='fas  fa-moon'></i><span style='color: white;'> x ".$InfoGT[55]."  </span>";											
+									   } 
 
 									 if (count($objlower) > 0)  {
 									  echo $InfoGT[67].$InfoGT[68].$InfoGT[69].$InfoGT[70].$InfoGT[71].$InfoGT[72].$InfoGT[73].$InfoGT[74];
-                                                                        echo " <small style='color: grey;'> <i class='fas  fa-sun'></i> <span style='color: white;'> x ". $InfoGT[41].$InfoGT[42].$InfoGT[43]. "</span> - <i class='fas  fa-moon'></i><span style='color: white;'> x ".$InfoGT[54].$InfoGT[55].$InfoGT[56]."</span>";  
-											}
-/*
-
-										 if (count($objlower) > 0)  {
-                                                                                echo $InfoGT[73].$InfoGT[74].$InfoGT[75].$InfoGT[76].$InfoGT[77].$InfoGT[78];
-                                                                                echo "<small style='color: grey;'><i class='fas  fa-sun'></i><span style='color: white;'>x". $InfoGT[47]. "</span> - <i class='fas  fa-moon'></i><span style='color: white;'>x".$InfoGT[60]."  </span>";
-										}
-
+									  echo " <small style='color: grey;'> <i class='fas  fa-sun'></i> <span style='color: white;'> x ". $InfoGT[41].$InfoGT[42].$InfoGT[43]. "</span> - <i class='fas  fa-moon'></i><span style='color: white;'> x ".$InfoGT[54].$InfoGT[55].$InfoGT[56]."</span>";  
+				  					   }
 */
 ?>
 
-
-	
-															</small></h5>
+														</small></h5>
 													<span class="description-text">GAME TIME</span>
 												</div>
 											</div>
@@ -251,14 +220,14 @@ $sec = "300";
 												<div class="description-block">
 											<h5 class="description-header">
 											  <?php  //echo count($objlower)
-
-                                                                                         if (count($objlower) == 0){     
+												echo $hive ;
+/*                                           if (count($objlower) == 0){     
 											 echo $InfoGT[18].$InfoGT[19].$InfoGT[20].$InfoGT[21].$InfoGT[22].$InfoGT[23].$InfoGT[24].$InfoGT[25];
 											 }
-
 											 if (count($objlower) > 0){
 											 echo $InfoGT[18].$InfoGT[19].$InfoGT[20].$InfoGT[21].$InfoGT[22].$InfoGT[23].$InfoGT[24].$InfoGT[25];
 											}
+*/											
 											 ?>
 
 													</h5>
@@ -283,25 +252,20 @@ $sec = "300";
 
 											<li class="nav-item">
 												<a class="nav-link active show" id="overview-tab" data-toggle="tab" href="#overview" role="tab" aria-controls="overview" aria-selected="true">
-													<!--<i class="fas fa-home">-->
-													<i class="fas fa-chart-area"></i> Server Info</a>
+												<!--<i class="fas fa-home">-->
+												<i class="fas fa-chart-area"></i> Server Info</a>
 											</li>
-											
 											<li class="nav-item">
-												 <a class="nav-link" id="modlist-tab" data-toggle="tab" href="#modlist" role="tab" aria-controls="modlist" aria-selected="false">
-													<i class="fas	fa-server"></i> MOD List</a>
+												<a class="nav-link" id="modlist-tab" data-toggle="tab" href="#modlist" role="tab" aria-controls="modlist" aria-selected="false">
+												<i class="fas	fa-server"></i> MOD List</a>
 											</li>											
-											
 											<li class="nav-item">
-                                                                                                 <a class="nav-link" id="userdata-tab"  data-toggle="tab" href="#userdata" role="tab" aria-controls="userdata" aria-selected="false">
-                                                                                                        <i class="fas fa-user-check"></i> User  Database</a>
-                                                                                        </li>
-
-
+                                                <a class="nav-link" id="userdata-tab"  data-toggle="tab" href="#userdata" role="tab" aria-controls="userdata" aria-selected="false">
+                                            <i class="fas fa-user-check"></i> User  Database</a>
+                                            </li>
 											<li class="nav-item"> <a class="nav-link" id="map-tab" data-toggle="tab"  href="#maptab" role="tab" aria-controls="maptab" aria-selected="false">
-                                                                                                <i class="fas fa-map"></i>  MAP</a>
-                                                                                        </li>
-
+                                                 <i class="fas fa-map"></i>  MAP</a>
+                                            </li>
 										</ul>
 
 										<div class="tab-content content_wrapper" id="tabs">
@@ -311,13 +275,11 @@ $sec = "300";
 
 												<div class="row" style="margin-left: 30px;">
 
-
 													 <div class="col-lg-2 col-sm-5">
-                                                                                                                <div class="callout callout-dark" style="padding:0">
+                                                            <div class="callout callout-dark" style="padding:0">
 															 <img src="<?php echo $imagemap; ?>" class="arrondie2" height="50px"  width="150px">
-
-                                                                                                                </div>
-                                                                                                        </div>
+															  </div>
+                                                     </div>
 	
 													<div class="col-lg-2 col-sm-5">
 														<div class="callout callout-light">
@@ -363,11 +325,10 @@ $sec = "300";
 															<small class="text-muted">Bans</small>
 															<br> <strong class="h4"  style="color: #F86C6B">0</strong>
 															<div class="chart-wrapper">
-                                                                                                                                <canvas id="sparkline-chart-1" width="100"      height="30"></canvas>
-                                                                                                                        </div>
+                                                              <canvas id="sparkline-chart-1" width="100"  height="30"></canvas>
+                                                            </div>
 														</div>
 													</div>
-
 
 												</div>
 
@@ -508,11 +469,12 @@ $sec = "300";
 														    <?php if( !empty( $Players ) ): ?>
 															   <div class="card-header">List players online: <?php echo  $Info['Players'] ; ?></div>
 																<div class="card-body">
-
+                                                                                                                           													
 																<?php foreach( $Players as $Player ): ?>
 																<table>
         															<tr>
-                                          									<td style="width:200px"> <span class="btn btn-large btn-primary">"Survivor" </span> </td>									    					        <td>&emsp; <span style='color:grey'>Playtime </span>>  <?php echo $Player[ 'TimeF' ]; ?> Minutes </td>
+                                                        									<td style="width:200px"> <span class="btn btn-large btn-primary">"Survivor" </span> </td>
+										    					        <td>&emsp; <span style='color:grey'>Playtime </span>>  <?php echo $Player[ 'TimeF' ]; ?> Minutes </td>
 																</tr>
 																</table>
 
