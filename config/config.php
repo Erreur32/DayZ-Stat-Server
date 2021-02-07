d09478caeef647c98e5dbb834eecb70d8f7d21ce
Erreur32 <postmaster@echosystem.fr>
2021-02-07 14:07:07 +0100

    first commit

--- config/config.php
+++ config/config.php
@@ -4,16 +4,11 @@
 // Need to  Edit this  
 
 
-	$ipserv   = "82.64.214.194" ; // IP server game
-	$servport = "3222" ; // Game Server Port
-	$modport  = "3232" ; // Mod port omega (+10)
-	$queryport= "27022" ; // Queryport
-/*
                 $ipserv   = "103.58.149.102" ; // IP server game
                 $portserv = "2302" ; // Game Server Port
 		$modport  = "2312" ; // Mod port omega (+10)
 		$queryport= "27016" ; // Queryport 
-*/
+
 	$title    = "DayZ Stat SERVER Clan | by TOX" ; // Web title page
 	$descript = "Your Clan/server desciption";  // Your clan/server description 
 	$logoteam = "logoteam.png";
